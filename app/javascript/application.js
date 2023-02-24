21736ccaf494d19ea5877c0c3de42489323f9ba2
Tiago Pinto <jotipinto@gmail.com>
2023-02-24 10:58:37 +0000

    smth

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "bootstrap"
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
